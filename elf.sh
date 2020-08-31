d1f25125553d8a2391245144b3277b167bf9be19
Faisal-97 <44176857+Faisal-97@users.noreply.github.com>
2020-08-31 17:38:27 +0300

    Update elf.sh

--- elf.sh
+++ elf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-kubectl --token=$TOKEN create -f namespace=elf
+kubectl --token=$TOKEN apply -f elf.namespace.yaml
 helm repo add elastic https://helm.elastic.co
 helm repo add fluent https://fluent.github.io/helm-charts
 helm repo update
